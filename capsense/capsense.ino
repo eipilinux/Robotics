8e5a90b30ab230c4e572ac81b78c2020b235142b
eipilinux <17235804+eipilinux@users.noreply.github.com>
2021-11-17 13:22:42 -0700

    increased sensativity, may need to add an auto adjustment feature where it overtime sets its own baseline and auto adjusts the cutoff value.

--- capsense/capsense.ino
+++ capsense/capsense.ino
@@ -16,7 +16,7 @@ int capSensePin = 2;
 //  by trial and error, or you could take readings at 
 //  the start of the program to dynamically calculate this.
 // If this is not sensitive enough, try a resistor with more ohms.
-int touchedCutoff = 100;
+int touchedCutoff = 90;
 
 void setup(){
   Serial.begin(9600);
