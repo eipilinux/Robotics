8bc497684f88ad534005863cbcceb6264770ed0f
eipilinux <smithmillpaine@gmail.com>
2022-04-01 16:23:18 -0600

    updated stuff

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -1,266 +1,129 @@
-int homepos = 1;
+/*
+* 
+* pin 7 ---> Tester 1 Pass (Pin is an input pin that will go HIGH when tester 1 cycle passes)
+* pin 6 ---> Tester 1 Cycle End (Pin is an input pin that will go HIGH when tester 1 cycle ends)
+* 
+* pin 5 ---> Tester 2 Pass (Pin is an input pin that will go HIGH when tester 2 cycle passes)
+* pin 4 ---> Tester 2 Cycle End (Pin is an input pin that will go HIGH when tester 2 cycle ends)
+* 
+* pin 2 ---> Tester 1 Start (HIGH signal starts tester 1, This pin needs to be set HIGH for at least 0.020 seconds to activate tester)
+* pin 3 ---> Tester 2 Start (HIGH signal starts tester 2, This pin needs to be set HIGH for at least 0.020 seconds to activate tester)
+* 
+* pin 9 ---> Welder Home Switch (This pin is the signal wire from a limit switch and will go HIGH when the welder is in the home position and LOW when the welder is welding)
+* pin 8 ---> Welder Start (This pin is normally set LOW and a HIGH signal starts the welder, This pin needs to be set HIGH for at least 0.700 seconds to activate welder)
+* 
+* ASCII digit 1 ---> BYTE value of 49 (this is what the Raspi will send over the serial comm to start tester 1)
+* ASCII digit 2 ---> BYTE value of 50 (this is what the Raspi will send over the serial comm to start tester 2)
+* ASCII Capital 'W' ---> BYTE value of 87 (this is what the Raspi will send over the serial comm to start the welder)
+* 
+*/
+
+//can be LOW (0) for welder home, or HIGH (1) for welder active
+int WELDER_STATE = 0;
+
+//welder
+const int welder_start = 8;
+const int welder_home = 9;
+
+//solo unit 1 (leak decay)
+const int solo_1_end = 6;
+const int solo_1_pass = 7;
+const int solo_1_start = 2;
+int SOLO_1_READY = 0; // is HIGH (1) if the system is ready for a new part and LOW (0) while testing
+int SOLO_1_STATE = 0; // holds a PASS (1) or FAIL (0) value
+
+//solo unit 2 (leak decay)
+const int solo_2_end = 4;
+const int solo_2_pass = 5;
+const int solo_2_start = 3;
+int SOLO_2_READY = 0; // is HIGH (1) if the system is ready for a new part and LOW (0) while testing
+int SOLO_2_STATE = 0; // holds a PASS (1) or FAIL (0) value
 
 void setup() {
-  Serial.begin(9600);
   /*
-   * pin 7 ---> Tester 1 Pass
-   * pin 6 ---> Tester 
-   * pin 5 ---> Tester 
-   * pin 4 ---> Tester 
-   * pin 3 ---> Tester 
-   * pin 2 ---> Tester 
-   * pin 9 ---> Welder Home Button
-   * pin 8 ---> Welder Start (HIGH signal starts welder, This pin needs to be set HIGH for at least 0.500 seconds to activate welder)
+   * We don't need crazy high baud rates because we will never be trying to time stuff to within 1/10,000th of a second 
+   * and so 9600 will be fine and will reduce the chances of transmission errors
    */
-}
+  Serial.begin(9600);
+  
+  pinMode(solo_1_start, OUTPUT);
+  pinMode(solo_2_start, OUTPUT);
+  pinMode(welder_start, OUTPUT);
 
-// the loop function runs over and over again forever
+  pinMode(solo_1_end, INPUT);
+  pinMode(solo_1_pass, INPUT);
+  
+  pinMode(solo_2_end, INPUT);
+  pinMode(solo_2_pass, INPUT);
+
+  pinMode(welder_home, INPUT);
+} //end of void setup{}
+
 void loop() {
-  if(digitalRead(8) == HIGH && homepos == 0){
-    Serial.println("Home");
-    homepos = 1;
+
+  //check the welder state
+  if(digitalRead(welder_home) == HIGH && WELDER_STATE == 1){
+    Serial.println("Welder Home");
+    WELDER_STATE = 0;
     delay(20);
   }
-  if(digitalRead(8) == LOW && homepos == 1){
-    homepos = 0;
+  if(digitalRead(welder_home) == LOW && WELDER_STATE == 0){
+    Serial.println("Welder Active");
+    WELDER_STATE = 1;
     delay(20);
   }
 
-//new section
-  if(digitalRead(3) == LOW && estop == 1){
-    Serial.println("estop");
-    estop = 0;
-    delay(20);
+  //check solo tester 1
+  if( SOLO_1_READY == 0 && digitalRead(solo_1_end) == HIGH){
+    if(digitalRead(solo_1_pass) == LOW)
+      Serial.println("1 Fail");
+    else if(digitalRead(solo_1_pass) == HIGH)
+      Serial.println("1 Pass");
+    SOLO_1_READY = 1;
   }
-  if(digitalRead(3) == HIGH && estop == 0){
-    Serial.println("resume");
-    estop = 1;
-    delay(20);
+  else if( SOLO_1_READY == 1 && digitalRead(solo_1_end) == LOW){
+    SOLO_1_READY = 0;
   }
-//
- 
+
+  //check solo tester 2
+  if( SOLO_2_READY == 0 && digitalRead(solo_2_end) == HIGH){
+    if(digitalRead(solo_2_pass) == LOW)
+      Serial.println("2 Fail");
+    else if(digitalRead(solo_2_pass) == HIGH)
+      Serial.println("2 Pass");
+    SOLO_2_READY = 1;
+  }
+  else if( SOLO_2_READY == 1 && digitalRead(solo_2_end) == LOW){
+    SOLO_2_READY = 0;
+  }
+  
+  //check the serial com
   if (Serial.available() > 0) {
     // read the incoming byte:
     byte incomingByte = Serial.read();
-    if(incomingByte == 49){
-      digitalWrite(7, HIGH);
-      //Serial.println("Received");
-      delay(1000);
-      digitalWrite(7, LOW);
+
+    //if the control computer sent 'W' (byte value of 87) then start the welder
+    if(incomingByte == 87){
+      digitalWrite(welder_start, HIGH);
+      Serial.println("Started Welder");
+      delay(700);
+      digitalWrite(welder_start, LOW);
     }
-  }
-}
 
-
-
-
-
-
-//data transfer variables
-int incomingByte = 0; // for incoming serial data
-
-//solo unit 1 (leak decay)
-const int solo_1_end = 10;
-const int solo_1_fail = 12;
-const int solo_1_pass = 11;
-const int solo_1_start = 2;
-int SOLO_1_READY; // is HIGH if the system is ready for a new part and LOW while testing
-int SOLO_1_STATE; // holds a PASS or FAIL value
-
-//solo unit 2 (leak decay)
-const int solo_2_end = A4;
-const int solo_2_fail = A2;
-const int solo_2_pass = A3;
-const int solo_2_start = 3;
-int SOLO_2_READY = 0; // is HIGH if the system is ready for a new part and LOW while testing
-int SOLO_2_STATE = 0; // holds a PASS or FAIL value
-
-const int table_home = A5;
-int TABLE_STATE;
-
-
-
-void setup() {
-  // initialize the solo test start pins as outputs:
-  pinMode(solo_1_start, OUTPUT);
-  pinMode(solo_2_start, OUTPUT);
-  pinMode(solo_3_start, OUTPUT);
-  
-  // initialize the solo tester pins as input:
-  pinMode(solo_1_end, INPUT);
-  pinMode(solo_1_fail, INPUT);
-  pinMode(solo_1_pass, INPUT);
-  //solo 2
-  pinMode(solo_2_end, INPUT);
-  pinMode(solo_2_fail, INPUT);
-  pinMode(solo_2_pass, INPUT);
-  //solo 3
-  pinMode(solo_3_end, INPUT);
-  pinMode(solo_3_fail, INPUT);
-  pinMode(solo_3_pass, INPUT);
-  //table home
-  pinMode(table_home, INPUT);
-
-  // opens serial port, sets data rate to 9600 bps
-  Serial.begin(9600); 
-
-  //do the initail check of the input pins for solo 1
-  if(digitalRead(solo_1_fail) == HIGH){
-    SOLO_1_STATE = 0;
-    Serial.println("Fail");
-  }
-  else if(digitalRead(solo_1_pass) == HIGH){
-    SOLO_1_STATE = 1;
-    Serial.println("Pass");
-  }
-  if(digitalRead(solo_1_end) == HIGH){
-    SOLO_1_READY = 1;
-    //Serial.println("1 is ready");
-  }
-  else{
-    SOLO_1_READY = 0;
-    //Serial.println("1 is active");
-  }
-
-  //do the initail check of the input pins for solo 2
-  if(digitalRead(solo_2_fail) == HIGH){
-    SOLO_2_STATE = 0;
-    Serial.println("Fail");
-  }
-  else if(digitalRead(solo_2_pass) == HIGH){
-    SOLO_2_STATE = 1;
-    Serial.println("Pass");
-  }
-  if(digitalRead(solo_2_end) == HIGH){
-    SOLO_2_READY = 1;
-    //Serial.println("2 is ready");
-  }
-  else{
-    SOLO_2_READY = 0;
-    //Serial.println("2 is active");
-  }
-
-  //do the initail check of the input pins for solo 3
-  if(digitalRead(solo_3_fail) == HIGH){
-    SOLO_3_STATE = 0;
-    Serial.println("Fail");
-  }
-  else if(digitalRead(solo_3_pass) == HIGH){
-    SOLO_3_STATE = 1;
-    Serial.println("Pass");
-  }
-  if(digitalRead(solo_3_end) == HIGH){
-    SOLO_3_READY = 1;
-    //Serial.println("3 is ready");
-  }
-  else{
-    SOLO_3_READY = 0;
-    //Serial.println("3 is active");
-  }
-
-  //do the initial check of table position
-  if(digitalRead(table_home) == HIGH){
-    TABLE_STATE = 1;
-    Serial.println("Table Home");
-  }
-  else{
-    TABLE_STATE = 0;
-    Serial.println("Table Moving");
-  }
-
-  //end of setup()
-}
-
-void loop() {
-  //check the input pins for solo tester 1
-  if( SOLO_1_READY == 0 && digitalRead(solo_1_end) == HIGH){
-      if(digitalRead(solo_1_fail) == HIGH || digitalRead(solo_1_pass) == LOW)
-        Serial.println("1 Fail");
-      else if(digitalRead(solo_1_pass) == HIGH)
-        Serial.println("1 Pass");
-      //Serial.println("1 is ready");
-      SOLO_1_READY = 1;
-  }
-  else if( SOLO_1_READY == 1 && digitalRead(solo_1_end) == LOW){
-    //Serial.println("1 is active");
-    SOLO_1_READY = 0;
-  }
-
-  //
-  //
-  //
-  
-  //check the input pins for solo tester 2
-  if( SOLO_2_READY == 0 && digitalRead(solo_2_end) == HIGH){
-      if(digitalRead(solo_2_fail) == HIGH || digitalRead(solo_2_pass) == LOW)
-        Serial.println("2 Fail");
-      else if(digitalRead(solo_2_pass) == HIGH)
-        Serial.println("2 Pass");
-      //Serial.println("2 is ready");
-      SOLO_2_READY = 1;
-  }
-  else if( SOLO_2_READY == 1 && digitalRead(solo_2_end) == LOW){
-    //Serial.println("2 is active");
-    SOLO_2_READY = 0;
-  }
-
-  //
-  //
-  //
-  
-  //check the input pins for solo tester 3
-  if( SOLO_3_READY == 0 && digitalRead(solo_3_end) == HIGH){
-      if(digitalRead(solo_3_fail) == HIGH || digitalRead(solo_3_pass) == LOW)
-        Serial.println("3 Fail");
-      else if(digitalRead(solo_3_pass) == HIGH)
-        Serial.println("3 Pass");
-      //Serial.println("3 is ready");
-      SOLO_3_READY = 1;
-  }
-  else if( SOLO_3_READY == 1 && digitalRead(solo_3_end) == LOW){
-    //Serial.println("3 is active");
-    SOLO_3_READY = 0;
-  }
-
-  //check the table position
-  if(TABLE_STATE == 0 && digitalRead(table_home) == HIGH){
-    TABLE_STATE = 1;
-    Serial.println("Table Home");
-  }
-  else if(TABLE_STATE == 1 && digitalRead(table_home) == LOW){
-    TABLE_STATE = 0;
-    Serial.println("Table Moving");
-  }
-
-  //deal with incoming commands
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-    incomingByte = Serial.read();
-
-    //start tester 1
-    if(incomingByte == 49){
+    //if the control computer sent '1' (byte value of 49) then start tester 1
+    else if(incomingByte == 49){
       digitalWrite(solo_1_start, HIGH);
-      //Serial.println("Received");
+      Serial.println("Started Tester 1");
       delay(20);
       digitalWrite(solo_1_start, LOW);
     }
 
-    //start tester 2
-    if(incomingByte == 50){
+    //if the control computer sent '2' (byte value of 50) then start tester 2
+    else if(incomingByte == 50){
       digitalWrite(solo_2_start, HIGH);
-      //Serial.println("Received");
+      Serial.println("Started Tester 2");
       delay(20);
       digitalWrite(solo_2_start, LOW);
     }
-
-    //start tester 3
-    if(incomingByte == 51){
-      digitalWrite(solo_3_start, HIGH);
-      //Serial.println("Received");
-      delay(20);
-      digitalWrite(solo_3_start, LOW);
-    }
-  }
-  
-  delay(1);
-}
+  } //end of serial.available{}
+} //end of void loop{}
