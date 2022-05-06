1d145141a7c9c6aafe7de1cef9908f6cfaf8bc2c
eipilinux <smithmillpaine@gmail.com>
2022-05-06 16:54:39 -0600

    update

--- temp.py
+++ temp.py
@@ -322,7 +322,7 @@ while True:
 		if i % 2 == 1:
 			if i > 1:
 				get_part_from_tester_1(roger)
-				put_in_correct_output(roger, 1)
+				put_in_correct_output(roger, tester_1_pass_or_fail)
 
 			get_part_from_welder(roger)
 			put_part_into_tester_1(roger, comport)
@@ -330,7 +330,7 @@ while True:
 		else:
 			if i > 1:
 				get_part_from_tester_2(roger)
-				put_in_correct_output(roger, 1)
+				put_in_correct_output(roger, tester_2_pass_or_fail)
 
 			get_part_from_welder(roger)
 			put_part_into_tester_2(roger, comport)
